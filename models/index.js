44058aa21ab92e0771423c0919ea8a683aca4e51
dainchatel <dain.chatel@gmail.com>
2020-05-27 22:56:29 -0400

    update sequelize connection for production

--- models/index.js
+++ models/index.js
@@ -3,17 +3,31 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 
-const dbConfig = {
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  dialect: 'postgres',
-}
+let db = {}
+let dbConfig
+let sequelize
 
-const db = {}
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, dbConfig)
+if (process.env.LOCAL) { 
+
+  dbConfig = {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  }
+  
+  sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, dbConfig)
+} else {
+
+  db = {}
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  })
+}
 
 fs
   .readdirSync(__dirname)
