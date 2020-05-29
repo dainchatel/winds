5eaa12333218fadfba188d839776209dd228419d
dainchatel <dain.chatel@gmail.com>
2020-05-28 22:02:15 -0400

    rm over optimized error message

--- middleware/defense.js
+++ middleware/defense.js
@@ -1,10 +1,10 @@
 const defense = (req, res, next) => {
-    const { ALLOWED_ORIGIN, ERROR_TEXT = 'Thanks for checking out my work.' } = process.env
+    const { ALLOWED_ORIGIN } = process.env
     try {
       const { origin } = req.headers
-      if (req.headers['postman-token']) throw new Error(ERROR_TEXT)
+      if (req.headers['postman-token']) throw new Error('Thanks for checking out my work.')
       if (!ALLOWED_ORIGIN) return next()
-      if (origin !== ALLOWED_ORIGIN) throw new Error(ERROR_TEXT)
+      if (origin !== ALLOWED_ORIGIN) throw new Error('Thanks for checking out my work.')
       return next()
     } catch (e) {
       console.log('i rejected the request')
